e5fb250358d12765680ed00d505e5d80794b61c2
Matthijs Blankevoort <matthijsblankevoort@mbpvanmatthijs.home>
2020-01-04 13:23:13 +0100

    feature: setup test suite

---
+++ TMessagesProj/src/main/test/TestSuite.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.telegram.messenger.SendMessagesHelper;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        SendMessagesHelper.class
+})
+
+public class  TestSuite {
+}
---
+++ TMessagesProj/src/main/test/org/telegram/test/SendMessagesHelperTest.java
@@ -0,0 +1,12 @@
+package org.telegram.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SendMessagesHelperTest {
+    @Test
+    public void testSendMessages() {
+        assertEquals(1,1);
+    }
+}
