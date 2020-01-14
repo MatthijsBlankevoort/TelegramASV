aeea1af46403916e535b4df139434428cbd49f49
Matthijs Blankevoort <matthijsblankevoort@hotmail.com>
2020-01-14 15:10:55 +0100

    test: update sendmessagehelpertest WIP

--- TMessagesProj/src/test/java/SendMessagesHelperTest.java
+++ TMessagesProj/src/test/java/SendMessagesHelperTest.java
@@ -1,5 +1,7 @@
 import android.accounts.Account;
 
+import com.google.android.exoplayer2.decoder.SimpleOutputBuffer;
+
 import net.hockeyapp.android.metrics.model.Base;
 
 import org.junit.Before;
@@ -10,24 +12,15 @@ import org.mockito.MockitoAnnotations;
 import org.telegram.messenger.AccountInstance;
 import org.telegram.messenger.BaseController;
 import org.telegram.messenger.MessageObject;
+import org.telegram.messenger.MessagesController;
 import org.telegram.messenger.SendMessagesHelper;
 import org.telegram.tgnet.TLRPC;
 
-import java.util.ArrayList;
-
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
 
 public class SendMessagesHelperTest {
-    @Mock
-    MessageObject messageObject;
-
-    @Mock
-    TLRPC.Message message;
-
-    @Mock
-    AccountInstance account;
-
     @Before
     public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
@@ -35,12 +28,22 @@ public class SendMessagesHelperTest {
 
     @Test
     public void testSendMessages() {
-        MessageObject m = mock(MessageObject.class);
-        message.dialog_id = 1;
-        m.messageOwner = message;
-        SendMessagesHelper sendMessagesHelper = mock(SendMessagesHelper.class);
-        sendMessagesHelper.sendMessage(m);
 
-        assertTrue(messageObject.isSent());
+        TLRPC.Message message = mock(TLRPC.Message.class);
+        MessageObject m = mock(MessageObject.class);
+        AccountInstance accountInstance = mock(AccountInstance.class);
+
+        message.from_id = 1;
+        message.post = false;
+        message.id = 123;
+        m.messageOwner = message;
+        m.messageOwner.dialog_id = 1;
+        m.messageOwner = message;
+
+        SendMessagesHelper sendMessagesHelper = accountInstance.getSendMessagesHelper();
+        System.out.println("SEND MESSAGES HELPER " + sendMessagesHelper);
+        doCallRealMethod().when(sendMessagesHelper).sendMessage(m);
+
+        assertTrue(m.isSent());
     }
 }
