2d378040a412e8a03cc9c0b27a5ce67e0dd16a2a
Matthijs Blankevoort <matthijsblankevoort@hotmail.com>
2020-01-14 16:54:19 +0100

    test: update sendmessagehelpertest WIP

--- TMessagesProj/src/test/java/SendMessagesHelperTest.java
+++ TMessagesProj/src/test/java/SendMessagesHelperTest.java
@@ -5,11 +5,15 @@ import org.mockito.MockitoAnnotations;
 import org.telegram.messenger.AccountInstance;
 import org.telegram.messenger.MessageObject;
 
+import org.telegram.messenger.MessagesController;
 import org.telegram.messenger.SendMessagesHelper;
 import org.telegram.tgnet.TLRPC;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 public class SendMessagesHelperTest {
@@ -23,22 +27,36 @@ public class SendMessagesHelperTest {
 
         TLRPC.Message message = mock(TLRPC.Message.class);
         MessageObject m = mock(MessageObject.class);
+        doCallRealMethod().when(m).getDialogId();
+
         AccountInstance accountInstance = mock(AccountInstance.class);
 
         message.from_id = 1;
         message.post = false;
         message.id = 123;
-        m.messageOwner = message;
-        m.messageOwner.dialog_id = 1;
+        message.dialog_id = 1;
+
         m.messageOwner = message;
 
         doCallRealMethod().when(accountInstance).getSendMessagesHelper();
-        
+
+        MessagesController messagesController = mock(MessagesController.class);
+
+        doCallRealMethod().when(accountInstance).getMessagesController();
+
+        doCallRealMethod().when(messagesController).getInputPeer(any());
+
+
+
+
         SendMessagesHelper sendMessagesHelper = accountInstance.getSendMessagesHelper();
 
         System.out.println("SEND MESSAGES HELPER " + sendMessagesHelper);
         sendMessagesHelper.sendMessage(m);
+
         System.out.println(m.messageOwner.send_state);
+        System.out.println(m.isSent());
+
         assertTrue(m.isSent());
     }
 }
