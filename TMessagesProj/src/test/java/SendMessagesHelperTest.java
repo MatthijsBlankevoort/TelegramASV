77f8560797a737afba9067325f35fac5a2332b0a
Farah Jama <farah@peakfijn.nl>
2020-01-21 17:31:11 +0100

    [test] more tests

--- TMessagesProj/src/test/java/SendMessagesHelperTest.java
+++ TMessagesProj/src/test/java/SendMessagesHelperTest.java
@@ -6,11 +6,14 @@ import org.telegram.messenger.AccountInstance;
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.ImageLoader;
+import org.telegram.messenger.MediaController;
 import org.telegram.messenger.MessageObject;
 
 import org.telegram.messenger.MessagesController;
 import org.telegram.messenger.SendMessagesHelper;
 import org.telegram.tgnet.TLRPC;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -63,6 +66,50 @@ public class SendMessagesHelperTest {
 
         m.messageOwner = message;
         m.emojiAnimatedSticker = document;
+
+
+        sendMessagesHelper.sendMessage(m);
+
+        assertTrue(m.isSent());
+    }
+
+    @Test
+    public void testSendAudioFiles() {
+        TLRPC.Message message = mock(TLRPC.Message.class);
+
+        TLRPC.TL_document document = mock(TLRPC.TL_document.class);
+        TLRPC.TL_documentAttributeAudio audio = mock(TLRPC.TL_documentAttributeAudio.class);
+        MessageObject messageObject = mock(MessageObject.class);
+        doCallRealMethod().when(messageObject).getDialogId();
+        mock(TLRPC.User.class);
+        AccountInstance accountInstance = mock(AccountInstance.class);
+
+
+        doCallRealMethod().when(accountInstance).getSendMessagesHelper();
+        SendMessagesHelper sendMessagesHelper = accountInstance.getSendMessagesHelper();
+        doCallRealMethod().when(messageObject).isSent();
+
+        if (messageObject.isVoice()) {
+            boolean result = MediaController.getInstance().playAudio(messageObject);
+            return result;
+        }
+    }
+
+    @Test
+    public void testChatIsEncrypted() {
+        TLRPC.Message message = mock(TLRPC.Message.class);
+        TLRPC.EncryptedChat encryptedChat = mock(TLRPC.EncryptedChat.class);
+        MessageObject m = mock(MessageObject.class);
+        doCallRealMethod().when(m).getDialogId();
+        mock(TLRPC.User.class);
+        AccountInstance accountInstance = mock(AccountInstance.class);
+
+        doCallRealMethod().when(accountInstance).getSendMessagesHelper();
+        SendMessagesHelper sendMessagesHelper = accountInstance.getSendMessagesHelper();
+        doCallRealMethod().when(m).isSent();
+
+        m.messageOwner = message;
+
 
 
         sendMessagesHelper.sendMessage(m);
