9198295dd66e6a15c32ef175fbb959f002a37341
Matthijs Blankevoort <matthijsblankevoort@hotmail.com>
2020-01-21 16:56:04 +0100

    refactor: rename chattest to contacttest

--- TMessagesProj/src/test/java/ContactTest.java
+++ TMessagesProj/src/test/java/ContactTest.java
@@ -15,7 +15,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
-public class ChatTest {
+public class ContactTest {
 
     @Test
     public void testUserCanAddContact() {
