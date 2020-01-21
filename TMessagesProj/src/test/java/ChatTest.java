d34ba8d9a40c87ab568b2f85eac3bc0e69699fe2
Matthijs Blankevoort <matthijsblankevoort@hotmail.com>
2020-01-21 16:44:19 +0100

    test: add test for add contact

---
+++ TMessagesProj/src/test/java/ChatTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.telegram.messenger.AccountInstance;
+import org.telegram.messenger.ChatObject;
+import org.telegram.messenger.ContactsController;
+import org.telegram.messenger.MessageObject;
+import org.telegram.messenger.SendMessagesHelper;
+import org.telegram.messenger.UserObject;
+import org.telegram.tgnet.TLRPC;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class ChatTest {
+
+    @Test
+    public void testUserCanAddContact() {
+        TLRPC.User user = mock(TLRPC.User.class);
+
+        ContactsController contactsController = mock(ContactsController.class);
+
+        contactsController.addContact(user, false);
+        when(contactsController.isContact(user.id)).thenReturn(true);
+        System.out.println(contactsController.isContact(user.id));
+
+        assertTrue(contactsController.isContact(user.id));
+    }
+}
